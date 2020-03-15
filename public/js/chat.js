c3ba938517f337d7e8663d876d83a8eec5baffb1
AvraamNikolaou <avnikolaou@hotmail.com>
2020-03-15 16:28:34 +0200

    feat: scroll to button when a new message is sent. Not when we scrolled up

--- public/js/chat.js
+++ public/js/chat.js
@@ -15,6 +15,31 @@ const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML;
 // Option
 const { username, room } = Qs.parse(location.search, {  ignoreQueryPrefix: true });
 
+const autoScroll = () => {
+    // New message element
+    const $newMessage = $messages.lastElementChild
+
+    // Height of the new message
+    const newMessageStyles = getComputedStyle($newMessage);
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+
+    console.log(newMessageHeight);
+
+    // Visible height
+    const visibleHeight = $messages.offsetHeight;
+
+    // Height of messages container
+    const containerHeight = $messages.scrollHeight;
+
+    // How far have i scrolled
+    const scrollOffset = $messages.scrollTop + visibleHeight;
+
+    if (containerHeight - newMessageHeight <= scrollOffset) {
+        $messages.scrollTop = $messages.scrollHeight;
+    }
+};
+
 socket.on('message', (message) => {
     console.log(message);
     const html = Mustache.render(messageTemplate, {
@@ -23,6 +48,7 @@ socket.on('message', (message) => {
         createdAt: moment(message.createdAt).format('HH:mm:ss')
     });
     $messages.insertAdjacentHTML('beforeend', html);
+    autoScroll();
 });
 
 socket.on('locationMessage', (message) => {
@@ -33,6 +59,7 @@ socket.on('locationMessage', (message) => {
         createdAt: moment(message.createdAt).format('HH:mm:ss')
     });
     $messages.insertAdjacentHTML('beforeend', html);
+    autoScroll();
 });
 
 socket.on('roomData',({ room, users }) =>{
